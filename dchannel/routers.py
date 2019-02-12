92508b3127304a5b530233bde9c4f8a97488a97f
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-12 21:30:55 +0530

    Update routers.py

--- dchannel/routers.py
+++ dchannel/routers.py
@@ -22,3 +22,18 @@ class ChatRouter(BaseRouter):
 
 
 route_handler.register(ChatRouter)
+
+class CompanyRouter(ModelRouter):
+    serializer_class = CompanySerializer
+    model = Company
+    route_name = 'company-route'
+    include_related = [StaffSerializer, DocumentSerializer, CompanyOwnerSerializer]
+
+    def get_query_set(self, **kwargs):
+        return self.model.objects.all()
+
+    def get_object(self, **kwargs):
+        return self.model.objects.get(pk=kwargs['pk'])
+
+
+route_handler.register(CompanyRouter)
