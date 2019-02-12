31e812edfb53cc4018d9fbd9a292ea4b7ba147b4
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-12 21:36:29 +0530

    Update models.py

--- dchannel/notifier/models.py
+++ dchannel/notifier/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Channel(models.Model):
+    user        = models.ForeignKey(User, null=True, blank=True)
+    links     = models.ManyToManyField(Courses, blank=True)
+    subtotal    = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    total       = models.DecimalField(default=0.00, max_digits=8, decimal_places=2)
+    updated     = models.DateTimeField(auto_now=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def is_digital(self):
+        qs = self.courses.all()
+        new_qs=  qs.filter(is_digital = False)
+        if new_qs.exists(): 
+            return False
+        return True
+
+ 
+
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        courses = instance.courses.all()
+        total = Decimal(0.00)
+        for x in courses:
+            total = Decimal(total) + Decimal(x.price)
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Channel.courses.through)
+
