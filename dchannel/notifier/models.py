0ac5111cf160ab2a9d1e0be38e7eed60296086d6
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-12 21:36:41 +0530

    Update models.py

--- dchannel/notifier/models.py
+++ dchannel/notifier/models.py
@@ -24,18 +24,3 @@ class Channel(models.Model):
         return True
 
  
-
-
-
-def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
-    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
-        courses = instance.courses.all()
-        total = Decimal(0.00)
-        for x in courses:
-            total = Decimal(total) + Decimal(x.price)
-        if instance.subtotal != total:
-            instance.subtotal = total
-            instance.save()
-
-m2m_changed.connect(m2m_changed_cart_receiver, sender=Channel.courses.through)
-
