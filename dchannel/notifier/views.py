6e9620e5ff7d22b35846fc9bf2a62ff46c41a304
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-14 20:27:01 +0530

    Update views.py

--- dchannel/notifier/views.py
+++ dchannel/notifier/views.py
@@ -4,3 +4,42 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'notifier/home.html'
+    
+    
+    class BroadcastChatView(TemplateView):
+    template_name = 'broadcast_chat.html'
+
+    def get(self, request, *args, **kwargs):
+        welcome = RedisMessage('Hello everybody')  # create a welcome message to be sent to everybody
+        RedisPublisher(facility='foobar', broadcast=True).publish_message(welcome)
+        return super(BroadcastChatView, self).get(request, *args, **kwargs)
+
+
+class UserChatView(TemplateView):
+    template_name = 'user_chat.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserChatView, self).get_context_data(**kwargs)
+        context.update(users=User.objects.all())
+        return context
+
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(UserChatView, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        redis_publisher = RedisPublisher(facility='foobar', users=[request.POST.get('user')])
+        message = RedisMessage(request.POST.get('message'))
+        redis_publisher.publish_message(message)
+        return HttpResponse('OK')
+
+
+class GroupChatView(TemplateView):
+    template_name = 'group_chat.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GroupChatView, self).get_context_data(**kwargs)
+        context.update(groups=Group.objects.all())
+        return context
+
+   
