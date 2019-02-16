db73ae42d9746f44a666905ca37103294e28fc67
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-16 08:10:10 +0530

    Update routing.py

--- dchannel/dchannel/routing.py
+++ dchannel/dchannel/routing.py
@@ -2,7 +2,20 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from notifier.consumers import EchoConsumer, Tick, NoseyConsumer
 
+application = ProtocolTypeRouter({
 
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            url(r"^chat/admin/$", AdminChatConsumer),
+            url(r"^chat/$", PublicChatConsumer),
+        ])
+    ),
+
+    # Using the third-party project frequensgi, which provides an APRS protocol
+    "aprs": APRSNewsConsumer,
+
+})
 
 
 application = ProtocolTypeRouter({
